0749fd4c01913c84619a36cc502cb877643360a3
Billy Proaño <billy97proano@hotmail.com>
2022-03-02 23:05:00 -0500

    Creamos nuestro modelo
    Existe un error interno del servidor

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request
+from sklearn.preprocessing import StandardScaler
+import pandas as pd 
+import pickle
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(modelo.labels_)
     return 'Hello world '
 
 
-@app.route('/predict')
+modelo = pickle.load(open('modelo.sav', 'rb'))
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    return 'Api de predicciones'
+
+    #cargar datos 
+    
+    df = request.get_json()
+    print(df)
+
+    #Realizamos el escalado de los datos 
+    ss = StandardScaler()
+    df = ss.fit_transform(df)
+    df = pd.DataFrame(df)
+    print(df)
+
+    #predecimos a que grupo pertenece
+    modelo.fit(df)
+
+    prediccion = map(modelo.labels_)
+
+    return prediccion
+        
+    
 
 if __name__ =="__main__":
     app.run()
